2b9465253c8c12219f6b7767bde6d373ac9d1794
Ruchir Toshniwal <ruchirtoshniwal450@gmail.com>
2020-05-27 16:49:15 +0530

    created app routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule, Routes } from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { CustomerComponent } from './customer/customer.component';
 import { InvoiceComponent } from './invoice/invoice.component';
 import { ProductComponent } from './product/product.component';
 
+
+const routes: Routes = [
+  { path: 'customer', component: CustomerComponent },
+  { path: 'invoice', component: InvoiceComponent },
+  { path: 'product', component: ProductComponent }
+];
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +22,8 @@ import { ProductComponent } from './product/product.component';
     ProductComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
